197295abb62e0ae6fb0a420b6f6c4ee0b35fccea
Zhaofeng Li <hello@zhaofeng.li>
2016-02-27 21:31:27 +0800

    AuthController: Set defaultMethod

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -119,6 +119,7 @@ class AuthController extends BaseController
         $user->pass = Hash::passwordHash($passwd);
         $user->passwd = Tools::genRandomChar(6);
         $user->port = Tools::getLastPort()+1;
+        $user->method = Config::get( "defaultMethod" );
         $user->t = 0;
         $user->u = 0;
         $user->d = 0;
