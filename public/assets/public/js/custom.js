ef452ddc0f87d96f4cbffb0165cbade41c9a674d
灰灰 <0w0@loli.pet>
2016-08-03 18:21:17 +0800

    add panel animate

--- public/assets/public/js/custom.js
+++ public/assets/public/js/custom.js
@@ -1,9 +1,15 @@
 $(document).ready(function () {
-
     $(".sidebar-menu a").each(function (num, ele) {
         if (location.href === ele.href)
             $(ele).addClass("sidebar-menu-current")
     });
-
-
+    var time = 0;
+    $(".panel").each(function (num, ele) {
+        setTimeout(function () {
+            $(ele).addClass('animated bounceInUp').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                $(ele).removeClass('animated bounceInUp');
+            });
+        }.bind(this), time);
+        time += 200;
+    });
 });
