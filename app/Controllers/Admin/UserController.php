0cb7708969b8d5aa4d579a822cadfbfdd5d92ca5
NeverBehave <luoxinhao77@gmail.com>
2016-11-13 10:13:42 +0800

    add telegram_id update

--- app/Controllers/Admin/UserController.php
+++ app/Controllers/Admin/UserController.php
@@ -54,6 +54,7 @@ class UserController extends AdminController
         $user->donate_amount = $request->getParam('donate_amount');
         $user->is_protected = $request->getParam('is_protected');
         $user->note = $request->getParam('note');
+        $user->telegram_id = $request->getParam('telegram_id');
         $acmod = false;
         if($user->ac_enable){
             if($user->ac_user_name != $request->getParam('ac_user_name')){
